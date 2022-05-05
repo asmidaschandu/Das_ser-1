cf306abd06022ce908a79ba8f29efc792e0354ca
asmidaschandu <94602456+asmidaschandu@users.noreply.github.com>
2022-05-05 09:01:36 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,9 +9,9 @@ Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (a
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Amalser [OWNER]\n' // full name
-            + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'FN: Das-Ser [OWNER]\n' // full name
+            + 'ORG:Das;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=919495944713:+91 9495944713\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "Das-Ser [OWNER]", vcard: vcard}, MessageType.contact)
 }))
